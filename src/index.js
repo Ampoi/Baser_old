8f9ed482c24b8309b608e779c0fa49785ca5f234
Ampoi <shibata.kuronosuke.offcial@gmail.com>
2022-08-03 12:54:36 +0900

    nedbの追加(前コミットの再コミット)

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ const app = express();
 const server = http.Server(app);
 const io = socketIo(server);
 
-const PORT = 3000;
+const PORT = 9000;
+//nedbデータベースセットアップ
+const Datastore = require("nedb")
+let chatDB = new Datastore({
+  filename: "./src/assets/chats.db",
+  autoload: true
+})
+chatDB.loadDatabase((error)=>{
+  if(error != null){console.error(error)}
+  console.log("loaded database");
+})
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -18,8 +28,17 @@ server.listen(PORT, () => {
 
 io.on('connection', (socket) => {
   console.log('user connected');
+  //メッセージが送られた時の処理
   socket.on('sendMessage', (message) => {
     console.log('Message has been sent: ', message);
-    io.emit('receiveMessage', message);
+
+    const newDoc = {
+      from: "testUser",
+      content: message
+    }
+    chatDB.insert(newDoc)
+
+    console.log(chatDB);
+    io.emit("updateList", newDoc)
   });
 });
